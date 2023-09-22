7d99a700bc2935ac2cfbe5cd463e83dd175cbf2d
komalChauhan99 <komalchauhan99931@gmail.com>
2023-09-22 12:53:17 +0530

    hey saumya

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+     {   
+          int num = 3;
+          int num1 = 7;
+            int   result=num+ num1;
+
+
+         System.out.print(result);
+        
+    }
+
+}
+  
